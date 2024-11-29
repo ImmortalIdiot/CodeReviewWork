ce028d7f9db321d6c44908a3180975329f856ad0
Nikita Savvin <nik.savvin.2004@mail.ru>
2024-11-29 19:14:31 +0300

    #1 - Refactor code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,22 +1,26 @@
 public class Calculator {
-    public int add(int a, int b){
+
+    public int add(int a, int b) {
         return a + b;
     }
-    public int dif(int a, int b){
+
+    public int dif(int a, int b) {
         return a - b;
     }
-    public int div(int a, int b){
+
+    public int div(int a, int b) {
         if (b == 0) {
             return 0;
-        }
-        else {
+        } else {
             return a / b;
         }
     }
-    public int times(int a, int b){
+
+    public int times(int a, int b) {
         return a * b;
     }
-    public int solver(int a, int b, int c, int d){
+
+    public int solver(int a, int b, int c, int d) {
         //My equation: ((a * b) - (a + c)) / d
         return div(dif(times(a, b), add(a, c)), d);
     }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,31 +1,32 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
+
     Calculator calculator = new Calculator();
 
     @Test
     void add() {
-        int result = calculator.add(5,3);
+        int result = calculator.add(5, 3);
         assertEquals(8, result);
     }
 
     @Test
     void dif() {
-        int result = calculator.dif(10,8);
+        int result = calculator.dif(10, 8);
         assertEquals(2, result);
     }
 
     @Test
     void div() {
-        int result = calculator.div(2,2);
+        int result = calculator.div(2, 2);
         assertEquals(1, result);
     }
 
     @Test
     void times() {
-        int result = calculator.times(5,2);
+        int result = calculator.times(5, 2);
         assertEquals(10, result);
     }
 
