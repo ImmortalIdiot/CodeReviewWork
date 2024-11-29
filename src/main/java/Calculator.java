88f8b8461fe1a2548f6f4abdae0d2c2d5e9c3c65
Nikita Savvin <nik.savvin.2004@mail.ru>
2024-11-29 18:56:18 +0300

    #1 - Add calculator with tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,23 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b == 0) {
+            return 0;
+        }
+        else {
+            return a / b;
+        }
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+    public int solver(int a, int b, int c, int d){
+        //My equation: ((a * b) - (a + c)) / d
+        return div(dif(times(a, b), add(a, c)), d);
     }
 }
